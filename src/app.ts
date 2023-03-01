2d64a99bd20dadefbc80c16654f7a523eb9be390
ShinyKiwis <nguyenhua.hcm@gmail.com>
2023-03-01 10:54:01 +0700

    Initialize repository for Github

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import * as express from "express"
+import * as bodyParser from "body-parser"
+import Controller from "interface/controller.interface";
+
+class App{
+  private app: express.Application;
+
+  constructor(controllers: Controller[]){
+    this.app = express()
+    this.initializeMiddleware()
+    this.initializeControllers(controllers)
+  }
+  
+  private initializeMiddleware() {
+    this.app.use(bodyParser.json())
+  }
+
+  private initializeControllers(controllers: Controller[]) {
+    controllers.forEach(controller => {
+      this.app.use("/api", controller.router)
+    })
+  }
+
+  public listen() {
+    this.app.listen(process.env.PORT, () => {
+      console.log(`Server listening on port: ${process.env.PORT}`)
+    })
+  }
+}
+
+export default App;
---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import App from "./app";
+import validateEnv from "./utils/validateEnv"
+import * as dotenv from "dotenv"
+
+dotenv.config()
+validateEnv()
+
+const app = new App(
+  [
+
+  ]
+)
+
+app.listen()
+
+
