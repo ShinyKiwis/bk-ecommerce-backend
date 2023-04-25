ff1a3cc7059c59e53ee6ed9d2c41a0980603df74
ShinyKiwis <nguyenhua.hcm@gmail.com>
2023-04-25 15:06:53 +0700

    Update: Fix server conflict

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-import { CategoryController } from "./controllers/category/category.controller";
-import App from "./app";
-import validateEnv from "./utils/validateEnv"
-import * as dotenv from "dotenv"
-
-dotenv.config()
-validateEnv()
-
-const app = new App(
-  [
-    new CategoryController()
-  ]
-);
-
-app.listen()
-=======
 import App from './app';
 import validateEnv from './utils/validateEnv';
 import * as dotenv from 'dotenv';
@@ -24,7 +7,6 @@ import ProductController from 'controllers/product/product.controller';
 
 dotenv.config();
 validateEnv();
->>>>>>> 4576618 (Update: Finish)
 
 const app = new App([
   new CategoryController(),
